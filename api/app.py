5e00dffd00a80726f6742d7d663db69e53a3850a
Imakshat47 <imakshat47@gmail.com>
2021-06-18 01:01:35 +0530

    Update

--- api/app.py
+++ api/app.py
@@ -46,4 +46,4 @@ def page_not_found(error):
 
 # Driver Method
 if __name__ == '__main__':
-    app.run(port=5000, debug=_app_debug)
+    app.run(port = int(os.environ.get('PORT', 33507)), debug=_app_debug)
